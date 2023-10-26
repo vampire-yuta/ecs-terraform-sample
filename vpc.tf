cef702cababf7d56346e1c18fa6d61eade195e4f
vampire-yuta <yuta@vamdemicsystem.black>
2023-10-26 18:11:48 +0900

    cloudwatch endpoint

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = true
 
   # [NOTE] PublicSubnetのみ作成している
@@ -59,6 +59,15 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
 resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id = module.vpc.vpc_id
   service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "logs_api" {
+  vpc_id = module.vpc.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
   vpc_endpoint_type   = "Interface"
   subnet_ids          = module.vpc.private_subnets
   security_group_ids  = [aws_security_group.vpc_endpoint.id]
